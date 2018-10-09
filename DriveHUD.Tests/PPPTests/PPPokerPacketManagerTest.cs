368fc71105b1945e774a7095ab99a9be2a7e3a04
Anatoly Ivakin <anatoly.ivakin.work@gmail.com>
2018-10-09 13:26:52 +0300

    Add PacketIsStartingPacketTest test for PPPokerPacketManager

---
+++ DriveHUD.Tests/PPPTests/PPPokerPacketManagerTest.cs
@@ -0,0 +1,34 @@
+﻿using DriveHUD.Tests.TcpImportersTests;
+using DriveHUD.Common.Extensions;
+using DriveHUD.Importers.AndroidBase;
+using DriveHUD.Importers.PPPoker;
+using DriveHUD.Importers.PPPoker.Model;
+//using Microsoft.QualityTools.Testing.Fakes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+//using System.Fakes;
+using System.IO;
+using System.Net;
+
+namespace DriveHUD.Tests.PPPTests
+{
+    [TestFixture]
+    class PPPokerPacketManagerTest : PacketManagerTest
+    {
+        protected override string TestDataFolder => "PPPTests\\TestData";
+
+        [TestCase(@"Packets\SngJoinREQ.txt", true)]
+        public void PacketIsStartingPacketTest(string file, bool expected)
+        {
+            var packetManager = new PPPokerPacketManager();
+
+            var bytes = ReadPacketFile(file);
+            var actual = packetManager.IsStartingPacket(bytes);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
